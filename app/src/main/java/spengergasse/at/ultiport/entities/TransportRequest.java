62bc77899911f960d736df4de7ba4823c792adea
mtrauner <tra17720@spengergasse.at>
2018-11-27 21:02:30 +0100

    Kommentare

--- app/src/main/java/spengergasse/at/ultiport/entities/TransportRequest.java
+++ app/src/main/java/spengergasse/at/ultiport/entities/TransportRequest.java
@@ -3,34 +3,54 @@ package spengergasse.at.ultiport.entities;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+
 import java.util.ArrayList;
 import java.util.Date;
 
-
-
+//Klasse für den Transportauftrag
 public class TransportRequest implements Parcelable {
 
-    private String reqID;
-    private String reqText;
-    private Anforderer reqAnforderer;
-    private Transporteur reqTransporteur;
-    private Date reqTime;
 
+    // ID des Auftrags, automatisch generiert in DB?
+    private String reqID;
+    //Beschreibung des Auftrags, wird beim Erstellen eingegeben
+    private String reqText;
+    //Ersteller des Auftrags, wird mitgespeichert beim Erstellen (aktiver User)
+    private Anforderer reqAnforderer;
+    //Zuständiger Transporteur, ist zunächst null und wird beim Annehmen des Auftrags auf den Trasnporteur gesetzt
+    private Transporteur reqTransporteur;
+    //Datum und Uhrzeit des Erstellens, wird mitgespeichert beim Erstellen
+    private Date reqTime;
+    //Start-Organisationseinheit beim Erstellen festgelegt
+    private Organsationseinheit reqOEStart;
+    //Start-Raum beim Erstellen festgelegt
+    private Raum reqRaumStart;
+    //Ziel-Organisationseinheit beim Erstellen festgelegt
+    private Organsationseinheit reqOETarget;
+    //Ziel-Raum beim Erstellen festgelegt
+    private Raum reqRaumTarget;
+
+
+    //Liste speichert alle Aufträge
     public static ArrayList<TransportRequest> requests = new ArrayList<TransportRequest>();
 
 
-
+    //Normaler Konstruktor TODO: Alle Attribute setzen wenn möglich
     public TransportRequest(String reqText, Date reqTime) {
         this.reqText = reqText;
         this.reqTime = reqTime;
 
     }
 
+    //Konstruktor für Parcelable zur Datenübergabe zwischen Activities TODO: Alle Attribute setzen wenn möglich
     public TransportRequest(Parcel in) {
+        //Setze Beschreibungs-Text auf eingelesenen String
         reqText = in.readString();
+        //Setze Zeit auf eingelesene Long
         reqTime = new Date(in.readLong());
     }
 
+    //Anforderung für Parcelable
     public static final Creator<TransportRequest> CREATOR = new Creator<TransportRequest>() {
         @Override
         public TransportRequest createFromParcel(Parcel in) {
@@ -43,6 +63,7 @@ public class TransportRequest implements Parcelable {
         }
     };
 
+    //Get & Set
     public String getReqID() {
         return reqID;
     }
@@ -90,15 +111,20 @@ public class TransportRequest implements Parcelable {
     public static void setRequests(ArrayList<TransportRequest> requests) {
         TransportRequest.requests = requests;
     }
+    //Get & Set Ende
 
+    //Anforderung für Parcelable
     @Override
     public int describeContents() {
         return 0;
     }
 
+    // Anforderung für Parcelable TODO: Alle Attribute setzen wenn möglich
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        //Übergebe Beschreibungs-Text
         dest.writeString(this.reqText);
+        //Übergebe Zeit als Long mit getTime(), da Date Datentyp nicht möglich
         dest.writeLong(this.reqTime.getTime());
     }
 }
