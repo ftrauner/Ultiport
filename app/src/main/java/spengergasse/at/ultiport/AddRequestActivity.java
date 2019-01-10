b2efbaa560afdbd1a72acaf8b99ee22ba9590934
Noah Benedict <cap18022@spengergasse.at>
2019-01-10 23:02:43 +0100

    User Edit funktioniert

--- app/src/main/java/spengergasse/at/ultiport/AddRequestActivity.java
+++ app/src/main/java/spengergasse/at/ultiport/AddRequestActivity.java
@@ -1,23 +1,63 @@
 package spengergasse.at.ultiport;
 
 import android.content.Intent;
+import android.net.http.RequestQueue;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
 
+import spengergasse.at.ultiport.entities.Raum;
 import spengergasse.at.ultiport.entities.TransportRequest;
 
 public class AddRequestActivity extends AppCompatActivity {
+
+    ArrayList<String> oeBez = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    Spinner sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_request);
+        Spinner spinnerA = findViewById(R.id.requestArt);
+        sp = (Spinner)findViewById(R.id.requestStartOE);
+        adapter=new ArrayAdapter<String>(this,R.layout.spinner_layout,R.id.txt,oeBez);
+        sp.setAdapter(adapter);
+
+        ArrayAdapter<CharSequence> adapterA = ArrayAdapter.createFromResource(this,
+                R.array.ArtDesTransports,R.layout.support_simple_spinner_dropdown_item);
+        adapterA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerA.setAdapter(adapterA);
+
+
 
         Toolbar administration_toolbar = findViewById(R.id.add_request_toolbar);
         setSupportActionBar(administration_toolbar);
@@ -25,6 +65,69 @@ public class AddRequestActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
+
+    public void onStart(){
+        super.onStart();
+        BackTask bt=new BackTask();
+        bt.execute();
+    }
+
+
+    private class BackTask extends AsyncTask<Void, Void, Void> {
+        ArrayList<String> list;
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            list = new ArrayList<>();
+        }
+
+        protected Void doInBackground(Void... params) {
+            InputStream is = null;
+            String result = "";
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost("http://ultiport.htl5.org/GetSpinnerData.php");
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                // Get our response as a String.
+                is = entity.getContent();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //convert response to string
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    result += line;
+                }
+                is.close();
+                //result=sb.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // parse json data
+            try {
+                JSONArray jArray = new JSONArray(result);
+                for (int i = 0; i < jArray.length(); i++) {
+                    JSONObject jsonObject = jArray.getJSONObject(i);
+                    // add interviewee name to arraylist
+                    list.add(jsonObject.getString("oe_bez"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void result) {
+            oeBez.addAll(list);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
 
     public void addReqClick(View view){
 
