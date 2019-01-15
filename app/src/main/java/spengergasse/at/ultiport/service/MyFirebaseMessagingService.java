1472724f8d114119d34ac1eca0d479c8e42887dc
trauner <tra17720@spengergasse.at>
2019-01-15 20:49:44 +0100

    Nutzergruppen fix

---
+++ app/src/main/java/spengergasse/at/ultiport/service/MyFirebaseMessagingService.java
@@ -0,0 +1,37 @@
+package spengergasse.at.ultiport.service;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FCM Service";
+    private static final String TOPIC_GLOBAL = "global";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+    }
+}
